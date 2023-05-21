665242c94a9d5a1234fb0a6522fc886ccb231653
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-21 18:49:58 +0530

    Added : Welcome message

---
+++ src/main/java/org/junit/Main.java
@@ -0,0 +1,8 @@
+package org.junit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to user registration problem using j-unit test!");
+    }
+}
